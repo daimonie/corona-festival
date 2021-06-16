7afcf4746fa45545cd296a6446a0d32b171905c1
Josko de Boer <joskodeboer@gmail.com>
2021-06-17 00:22:07 +0200

    Festival data analysed, but undersampled

--- container/app.py
+++ container/app.py
@@ -7,9 +7,13 @@ import theano.tensor as tt
 import pymc3 as pm
 from datetime import datetime
 from corona_festival.utils import setup_log, savefig, startfig, stop
+from corona_festival.model import beta_normal_model
 
 filenames = {
-    "raw": 'data/raw_data.csv'
+    "raw": 'data/raw_data.csv',
+    "explore": 'figures/explore_{situation}.png',
+    "burned_trace": 'figures/burned_trace_{situation}.png',
+    "burned_trace_csv": 'figures/burned_trace_{situation}.csv'
 }
 
 
@@ -20,10 +24,51 @@ if __name__ == "__main__":
 
     sns.set_theme(color_codes=True)
 
-    df_raw = pd.read_csv(
+    df_festivals = pd.read_csv(
             filenames['raw'],
             sep=',',
-            header=2
+            header=1
         )
 
-    logging.info(df_raw.head())
+    logging.info(df_festivals.head())
+    
+    for situation in df_festivals.situation.unique():
+        startfig()
+        sns.regplot(data=df_festivals[df_festivals.situation==situation], x='visitors', y='infected')
+        savefig(filenames['explore'].format(
+            situation=situation
+        ))
+
+        # This information was given to us.
+        false_negative = 0.20
+        # these two numbers were extracted 11 June 2021
+        real_num_corona = 61000
+        real_population_number = 17200000
+
+        # Model says p * N * false_negative come in infected. They each infect R people and so we end up with
+        # p N fn R people afterwards.
+        # p sounds like the parameter of a binomial, let's make it a beta distribution?
+
+        burned_trace = beta_normal_model(
+            data=df_festivals,
+            situation=situation,
+            hyper_p_mu=real_num_corona/real_population_number,
+            hyper_p_sigma=2*real_num_corona/real_population_number,
+            hyper_r_mu=3,
+            hyper_r_sigma=.2,
+            false_negative=false_negative,
+            sample=10000,
+            cores=6,
+            chains=4
+        )
+
+        startfig()
+        plt.title("Burn trace.")
+        pm.plot_trace(burned_trace)
+        savefig(filenames['burned_trace'].format(
+            situation=situation
+        )) 
+
+        pm.trace_to_dataframe(burned_trace).to_csv(filenames['burned_trace_csv'].format(
+            situation=situation
+        ))
--- container/corona_festival/model.py
+++ container/corona_festival/model.py
@@ -2,3 +2,61 @@ import logging
 import pymc3 as pm
 import pandas as pd
 import numpy as np
+import theano.tensor as tt
+
+def beta_normal_model(
+        data=None,
+        situation='cookies',
+        hyper_p_mu=0.01,
+        hyper_p_sigma=0.03,
+        hyper_r_mu=3,
+        hyper_r_sigma=10,
+        false_negative=.1,
+        sample=1000,
+        chains=4,
+        cores=4,
+        tune=1000,
+        nuts=False
+    ):
+    assert data is not None
+
+    logging.info(f"Bayesian stats analysis for {situation} festival data.")
+    df_data = data[data.situation == situation]
+
+    data_visitors = df_data.visitors.values
+    data_infected = df_data.infected.values
+
+    with pm.Model() as model:
+        r = pm.Normal(
+            'r',
+            mu = hyper_r_mu,
+            sigma = hyper_r_sigma
+        )
+        p = hyper_p_mu
+
+        lam = p * r * false_negative 
+        lam_print = tt.printing.Print("lam")(lam)
+
+        observed = pm.Normal(
+            "observed",
+            mu=lam* data_visitors,
+            sigma=50,
+            observed=data_infected
+        )
+
+        start = pm.find_MAP()
+        if nuts:
+            step = pm.NUTS()
+        else:
+            step = pm.Metropolis()
+        trace = pm.sample(
+            sample,
+            step=step,
+            start=start,
+            chains=chains,
+            cores=cores,
+            tune=tune
+        )
+        burned_trace = trace[int(sample*4/5):]
+
+    return burned_trace
